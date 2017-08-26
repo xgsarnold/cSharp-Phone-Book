d75cd4d42aa7e2e986ec3f0e228270aea692307d
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2017-08-26 17:54:40 -0500

    Added Contact model with initalizing method and properties

---
+++ PhoneBook/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Models
+{
+    public class Contact
+    {
+        public Contact(string lastName, string firstName, string phoneNumber)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            PhoneNumber = phoneNumber;
+        }
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
